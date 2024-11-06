69023ec19d5a19b074f34a08c99bb76544b5fce3
PeriklisCharalampidis <periklis.charalampidis@hotmail.com>
2024-11-06 14:00:40 +0100

    Reuse Dialog

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,6 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/core/Fragment"
-],function (Controller, MessageToast, Fragment) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
@@ -16,27 +14,8 @@ sap.ui.define([
         MessageToast.show(sMsg);
       },
       onOpenDialog: function () {
-        var oView = this.getView();
-
-        // create dialog lazily
-        if (!oView.byId("helloDialog")) {
-          // load asynchronous XML fragment
-          Fragment.load({
-            id: oView.getId(),
-            name: "sap.ui.demo.walkthrough.view.HelloDialog",
-            controller: this
-          }).then(function (oDialog) {
-            // connect dialog to the root view of this component (models, lifecycle, events)
-            oView.addDependent(oDialog);
-            oDialog.open();
-          });
-        } else {
-          this.byId("helloDialog").open();
-        } 
+        this.getOwnerComponent().openHelloDialog();
       },
-      onCloseDialog: function () {
-        this.byId("helloDialog").close();
-      }
     });
   }
 );
